cbfef577788cfd8bf5548075ca0705fc912db40d
Jose Gale <josedavidgale@gmail.com>
2021-01-22 19:50:19 -0600

    moving images

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import { Button } from "@material-ui/core";
 import { useEffect, useState } from "react";
-import { Interface } from "readline";
+import Image from "next/image";
 
 const steps = {
   WORK: { id: 1, mm: 25 },
@@ -142,14 +142,42 @@ export default function Home() {
         <meta name="keywords" content="Keywords" />
 
         <link rel="manifest" href="/manifest.json" />
-        <link rel="icon" type="image/png" href="/iconx/favicon-16x16.png" sizes="16x16"/>
-        <link rel="icon" type="image/png" href="/iconx/favicon-32x32.png" sizes="32x32"/>
-        <link rel="icon" type="image/png" href="/iconx/android-192x192.png" sizes="192x192"/>
-        <link rel="apple-touch-icon" href="/iconx/apple-touch-icon-180x180.png" sizes="180x180"/>
-        <meta name="msapplication-config" content="/iconx/browserconfig.xml"></meta>
+        <link
+          rel="icon"
+          type="image/png"
+          href="/iconx/favicon-16x16.png"
+          sizes="16x16"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          href="/iconx/favicon-32x32.png"
+          sizes="32x32"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          href="/iconx/android-192x192.png"
+          sizes="192x192"
+        />
+        <link
+          rel="apple-touch-icon"
+          href="/iconx/apple-touch-icon-180x180.png"
+          sizes="180x180"
+        />
+        <meta
+          name="msapplication-config"
+          content="/iconx/browserconfig.xml"
+        ></meta>
         <meta name="theme-color" content="#317EFB" />
       </Head>
       <div className={styles.header}>
+        <Image
+          src="/tile70x70.png"
+          alt="Picture of the author"
+          width={72}
+          height={32}
+        />
         <h2 className={styles.title}>Pomodoro Timer</h2>
       </div>
       <main className={`${styles.main}`}>
